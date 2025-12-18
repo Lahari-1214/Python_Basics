e0be01224ea2c208252ccd8c3f0ef7771af2b19e
Leela <leelanarayan1214@gmail.com>
2025-12-18 12:05:41 +0530

    Operators

---
+++ Operators/Membership.py
@@ -0,0 +1,16 @@
+"""
+The membership operators are useful to test for membership in a sequence such as 
+strings, lists, tuples or dictionaries
+1) in
+2) not in
+"""
+# Working of in operator
+names = ["Rani", "Yamini", "Sushmita", "Veena"]
+for name in names: 
+    print (name) 
+
+# Working of not in operator
+postal = {'Delhi': 110001, 'Chennai': 600001, 'Kolkata': 700001, 
+'Bangalore':560001} 
+for city in postal: 
+    print(city, postal[city]) 
---
+++ Operators/logical.py
@@ -0,0 +1,9 @@
+x = 100 
+False 
+y = 200 
+print(x and y)  # will display 200 
+print(x or y)  # will display 100 
+print (not x) # will display False 
+x=1;  y=2; z=3 
+if(x<y and y<z): print('Yes') 
+else: print('No') 
