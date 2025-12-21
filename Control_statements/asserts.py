303d6e3d0f55f3d4ccbb131cc4b880d086e233fb
Leela <leelanarayan1214@gmail.com>
2025-12-21 22:15:28 +0530

    Asserts

---
+++ Control_statements/asserts.py
@@ -0,0 +1,5 @@
+# The assert statement is useful to check if a particular condition is fulfilled or not. 
+# understanding assert statement 
+x = int(input('Enter a number greater than 0: ')) 
+assert x>0, "Wrong input entered" 
+print('U entered: ',x) 
