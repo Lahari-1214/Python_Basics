174b2e8422005e0017f7e6e2a50f5aab061bc071
Leela <leelanarayan1214@gmail.com>
2025-12-21 22:06:01 +0530

    pass

---
+++ Control_statements/pass.py
@@ -0,0 +1,13 @@
+'''
+The pass statement does not do anything. It is used with 'if' statement or inside a loop to 
+represent no operation. We use pass statement when we need a statement syntactically 
+but we do not want to do any operation.  
+'''
+# Using pass to do nothing 
+x = 0 
+while x<10:   
+    x+=1      
+    if x>5:  # if x > 5 then continue next iteration 
+        pass 
+    print ('x= ', x) 
+print("Out of loop")    
