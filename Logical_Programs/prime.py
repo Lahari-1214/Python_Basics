88ae575da002592c3e8fe5caf480ed8b3151691d
Leela <leelanarayan1214@gmail.com>
2025-12-22 15:39:30 +0530

    Prime Number series

--- Logical_Programs/prime.py
+++ Logical_Programs/prime.py
@@ -1,7 +1,7 @@
 # program to print prime numbers upto a given number 
 # accept upto what number the user wants 
 max = int(input("Upto what number? ")) 
- 
+
 for num in range(2, max+1):    # generate from 2 onwards till max 
     for i in range(2,num):  # i represents numbers from 2 to num-1 
         if (num % i) == 0:  # if num is divisible by i 
@@ -12,16 +12,24 @@ for num in range(2, max+1):    # generate from 2 onwards till max
 
     # Checking wheather your number is prime or not
 
-num = int(input("Enter the number"))
-result = 'false'
-for n in range(2,num+1):
-    for m in range(2,n):
-        if n%m == 0:
-            result = 'false'
-            break
-    else:
-        result = 'true'
-if result == 'true':
-    print("Your number is prime number")
+# num = int(input("Enter the number"))
+# result = 'false'
+# for n in range(2,num+1):
+#     for m in range(2,n):
+#         if n%m == 0:
+#             result = 'false'
+#             break
+#     else:
+#         result = 'true'
+# if result == 'true':
+#     print("Your number is prime number")
+# else:
+#     print("Your number is not a prime number")
+
+n = int(input("Enter the number"))
+for i in range(2,n//2+1):
+    if n%2 == 0:
+        print('It is not a pm')
+        break
 else:
-    print("Your number is not a prime number")
+    print("It is a prime")
