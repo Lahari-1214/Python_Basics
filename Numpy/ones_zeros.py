102d78e167b7475e264bef3c11217503e511fb41
Leela <leelanarayan1214@gmail.com>
2025-12-22 19:29:14 +0530

    playing with zeros and ones using numpy

---
+++ Numpy/ones_zeros.py
@@ -0,0 +1,13 @@
+# Creating Arrays using zeros() and ones() Functions
+'''
+zeros() function to create an array with all zeros. 
+The ones() function is useful to create an array with all 1s.
+'''
+from numpy import *
+a = zeros(10,int)
+print(a)
+
+b = ones(10,int) #default float
+print(b)
+
+
