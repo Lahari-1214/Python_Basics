8a39acaff43be83b527aa827991b69afdb3e6852
Leela <leelanarayan1214@gmail.com>
2025-12-23 11:57:12 +0530

    Strings

---
+++ Strings/creating_string.py
@@ -0,0 +1,16 @@
+# String represents a group of characters and str represents string in python
+
+s1 = 'Welcome to "Core Python" learning'  
+print(s1)
+
+# Length of a string: represents how many characters are there in a string we use len()
+str = "Leela kanthi"
+s = len(str)
+print(f"The given string is '{str}' and its length is '{s}'")
+
+# Indexing of a String
+# 1.str[0] refers to the 0th element of the string and str[1] refers to the 1st element of the string.
+# 2.str[-1] refers to the last element and str[-2] refers to second element from last
+
+print(str[0],str[3])
+print(str[-2],str[-1])
