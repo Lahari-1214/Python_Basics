c6bd6934d3c082eeef3b955e0de6619c9b071996
Leela <leelanarayan1214@gmail.com>
2025-12-25 11:23:39 +0530

    Variable Scope

---
+++ Functions/local_And_Global.py
@@ -0,0 +1,29 @@
+# local variable is a variable whose scope is limited within the function where it is created
+def myfun():
+    a = 1
+    a+=1
+    print(a)
+myfun()
+# print(a) #error raised because a is created inside the function
+
+
+# Global variable: when we declare the variable above the function it becomes a global variable
+var = "Global varible"
+def myfun2():
+    print(var)
+myfun2()
+
+
+# Global keyword:the global keyword is used inside a function to indicate that a variable refers to the global scope (outside the function), rather than creating a new local variable.
+a = 1
+def myfunction(): 
+    global a  
+ # this is global var 
+    print('global a= ', a)   # display global var 
+    a=2  
+ # modify global var value 
+    print('modifed a= ', a)  
+ # display new value 
+myfunction() 
+print('global a= ', a)  
+ # display modified value
