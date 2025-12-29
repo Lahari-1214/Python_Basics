d6003551f919aa359b1bde5d456485b8fd9049bc
Leela <leelanarayan1214@gmail.com>
2025-12-29 22:41:05 +0530

    Operations on Lists

---
+++ Lists_And_Tuples/operationsOnLists.py
@@ -0,0 +1,36 @@
+# a general way to create lists  
+# create a list with integer numbers 
+num = [10, 20, 30, 40, 50]  
+print('Total list= ', num)  # display total list 
+print('First= %d, Last= %d' % (num[0], num[4]))  # display first and   last elements 
+ 
+# create a list with strings 
+names = ["Raju", "Vani", "Gopal", "Laxmi"] 
+print('Total list= ', names)  # display entire list 
+print('First= %s, Last= %s' % (names[0], names[3]))  # display first   and last elements 
+ 
+# create a list with different elements 
+x = [10, 20, 10.5, 2.55, "Ganesh", 'Vishnu'] 
+print('Total list= ', x)  # display entire list 
+print('First= %d, Last= %s' % (x[0], x[5]))  # display first and last   elements 
+
+
+# 5-basic operation
+''' 
+1)Finding length
+2)Concatenation
+3)repetition
+4)membership
+5)iteration
+'''
+print(len(x)) #finding length
+
+print(num+names) #concatenation
+
+print(names*2) #repetition
+
+y= 'Raju' in names #membership
+print(y)
+
+for i in x:  #iteration
+    print(i)
