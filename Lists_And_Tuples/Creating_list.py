dba150c9c631dbf965bd85d5decdc56d8471cd9a
Leela <leelanarayan1214@gmail.com>
2025-12-29 22:20:37 +0530

    List creation

---
+++ Lists_And_Tuples/Creating_list.py
@@ -0,0 +1,27 @@
+# List: Lists are ordered collections of elements, Lists are mutable, meaning their elements can be changed,Lists are defined using square brackets [],Elements in a list can be of different data types.
+
+# Creating list
+student = [10, 'Venu gopal', 'M', 50, 55, 62, 74, 66] 
+e_lst = []   # this an empty list 
+print(student) 
+
+#  A Python program to create lists with different types of elements. 
+# a general way to create lists  
+# create a list with integer numbers 
+num = [10, 20, 30, 40, 50]  
+print('Total list= ', num)  # display total list 
+print('First= %d, Last= %d' % (num[0], num[4]))  # display first and  
+  # last elements 
+ 
+# create a list with strings 
+names = ["Raju", "Vani", "Gopal", "Laxmi"] 
+print('Total list= ', names)  # display entire list 
+print('First= %s, Last= %s' % (names[0], names[3]))  # display first  
+  # and last elements 
+ 
+# create a list with different elements 
+x = [10, 20, 10.5, 2.55, "Ganesh", 'Vishnu'] 
+print('Total list= ', x)  # display entire list 
+print('First= %d, Last= %s' % (x[0], x[5]))  # display first and last  
+  # elements
+
