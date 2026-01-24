8d7fd616c3e628fe20380f9f88d0777b05d50561
Leela <leelanarayan1214@gmail.com>
2026-01-24 17:29:03 +0530

    Introduction to Dictionaries

---
+++ Dictionary/creatingdict.py
@@ -0,0 +1,7 @@
+# A dictionary represents a group of elements arranged in the form of key-value pairs, enclosed inside the curly braces {}
+dict = {'Name': 'Chandra', 'Id': 200, 'Salary': 9080.50} 
+# access value by giving key 
+print('Name of employee= ', dict['Name']) 
+print('Id number= ', dict['Id']) 
+print('Salary= ', dict['Salary']) #the first element is considered as ‘key’ and the immediate next 
+#element is taken as its ‘value’. The key and its value are separated by a colon (:)
