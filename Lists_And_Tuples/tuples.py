b1931683e38269ae00a8dbff89774d77f136ba94
Leela <leelanarayan1214@gmail.com>
2026-01-24 14:33:37 +0530

    Creating tuples

---
+++ Lists_And_Tuples/tuples.py
@@ -0,0 +1,8 @@
+# Tuples are sequence types in python which stores data or items(immutable)
+# we can't perform append(),extend(), append(), extend(), insert(), remove(), pop() and clear() on tuples because tuples are immutable
+
+# Creating Tuples
+tup1 = ()  # empty touple 
+tup2 = (10,)  # tuple with one element. Observe comma after the element.
+tpl= tuple(range(4, 9, 2)) # numbers from 4 to 8 in steps of 2 
+print(tpl) 
